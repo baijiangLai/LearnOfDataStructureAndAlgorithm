64038136afc4325697b40f28b564c64d6c600b9d
laibaijiang <laibaijiang@lixiang.com>
2023-02-15 16:00:59 +0800

    init commit

---
+++ LinkedList/src/main/java/com/laibaijiang/RemoveNthFromEnd.java
@@ -0,0 +1,86 @@
+package com.laibaijiang;
+
+/**
+ * 本题题目条件：
+ * 1. 只给了一个完整的链表
+ * 2. 需要删除倒数指定的结点
+ */
+
+import java.util.LinkedList;
+
+/**
+ * 两种方法：正向的和逆向的
+ * 当然还可以使用栈
+ */
+
+public class RemoveNthFromEnd {
+    public static ListNode remove(ListNode head, int n) {
+        int len = 0;
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode p = dummy.next;
+        while(p != null){
+            len++;
+            p = p.next;
+        }
+        p = dummy;
+        int dif = len - n;
+        while(dif > 0){
+            p = p.next;
+            dif--;
+        }
+        ListNode q = p.next.next;
+        p.next = q;
+        return dummy.next;
+    }
+    public static ListNode remove2(ListNode head, int n) {
+        ListNode reverseNode = reverse(head);
+        ListNode dummy = new ListNode();
+        dummy.next = reverseNode;
+        ListNode p = dummy;
+        while(n - 1 > 0){
+            p = p.next;
+            n--;
+        }
+        p.next = p.next.next;
+        return reverse(dummy.next);
+    }
+    private static ListNode reverse(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode p = head, q = head.next, h = null;
+        while(q != null){
+            h = q.next;
+            q.next = p;
+            p = q;
+            q = h;
+        }
+        head.next = null;
+        head = p;
+        return head;
+    }
+    public static ListNode remove3(ListNode head, int n){
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode p = dummy;
+        LinkedList<ListNode> stack = new LinkedList<>();
+        while(p != null){
+            stack.push(p);
+            p = p.next;
+        }
+        for(int i = 0; i  < n; i++){
+            stack.pop();
+        }
+        ListNode q = stack.peek();
+        q.next = q.next.next;
+        return dummy.next;
+    }
+}
+
+/**
+ * 时间复杂度: O(len)
+ * 空间复杂度: O(1)
+ *
+ * 使用栈的时间复杂度和空间复杂度: O(len)，因为使用了额外空间栈
+ */
---
+++ LinkedList/src/main/java/com/laibaijiang/TwoSum.java
@@ -0,0 +1,116 @@
+package com.laibaijiang;
+
+
+/**
+ * [本题](https://leetcode.cn/problems/add-two-numbers/)条件：
+ * 1. 两个非空的链表
+ * 2. 数字是逆序存储的
+ * 3. 数字不会以0开头
+ */
+
+
+public class TwoSum {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode p = dummy;
+        int sum = 0, carry = 0;
+        while(l1 != null && l2 != null){
+            sum += l1.val + l2.val + carry;
+            if(sum > 9){
+                carry = sum / 10;
+                sum -= 10;
+            } else {
+                carry = 0;
+            }
+            ListNode s = new ListNode(sum);
+            p.next = s;
+            p = p.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        while(l1 != null){
+            sum += l1.val + carry;
+            if(sum > 9){
+                carry = sum / 10;
+                sum = sum - 10;
+            } else {
+                carry = 0;
+            }
+            ListNode s= new ListNode(sum);
+            p.next = s;
+            p = p.next;
+            l1 = l1.next;
+        }
+        while(l2 != null){
+            sum += l2.val + carry;
+            if(sum > 9){
+                carry = sum / 10;
+                sum = sum - 10;
+            } else {
+                carry = 0;
+            }
+            ListNode s= new ListNode(sum);
+            p.next = s;
+            p = p.next;
+            l2 = l2.next;
+        }
+        if(carry > 0){
+            ListNode s = new ListNode(carry);
+            p.next = s;
+            p = p.next;
+        }
+        return dummy.next;
+    }
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode p = dummy;
+        int sum = 0, carry = 0;
+        while(l1 != null || l2 != null){
+            int n1, n2;
+            if(l1 == null){
+                n1 = 0;
+            }else {
+                n1 = l1.val;
+            }
+            if(l2 == null){
+                n2 = 0;
+            }else {
+                n2 = l2.val;
+            }
+            sum = n1 + n2 + carry;
+            if(sum > 9){
+                carry = sum / 10;
+                sum -= 10;
+            } else {
+                carry = 0;
+            }
+            ListNode s = new ListNode(sum);
+            p.next = s;
+            p = p.next;
+            if(l1 != null) {//道理同l2
+                l1 = l1.next;
+            }
+            if(l2 != null) { //这里不能写写l2.next因为一旦某一个链表走到最后了，那么是无法将最后一个结点算在内的，该链表一直会走不完
+                l2 = l2.next;
+            }
+        }
+
+        if(carry > 0){
+            ListNode s = new ListNode(carry);
+            p.next = s;
+            p = p.next;
+        }
+        return dummy.next;
+    }
+}
+/**
+ * 两个解法对应了不同的写法
+ * 其核心的思想还是链表的指针移动，对于链表来说最关键的是记录下一个指针的信息
+ */
+
+/**
+ * 时间复杂度：O(max(len1, len2)
+ * 空间复杂度：O(1)
+ */
+
